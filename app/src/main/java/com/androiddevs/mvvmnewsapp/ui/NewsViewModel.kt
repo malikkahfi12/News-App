59c1d8ca11d8ac4660a774db5178129225651568
malikkahfi12 <malikkahfi1996@gmail.com>
2020-05-19 07:20:58 +0700

    first commit

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsViewModel.kt
@@ -0,0 +1,84 @@
+package com.androiddevs.mvvmnewsapp.ui
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.androiddevs.mvvmnewsapp.model.Article
+import com.androiddevs.mvvmnewsapp.model.NewsResponse
+import com.androiddevs.mvvmnewsapp.repository.NewsRepository
+import com.androiddevs.mvvmnewsapp.util.Resource
+import kotlinx.coroutines.launch
+import retrofit2.Response
+
+class NewsViewModel(
+    val newsRepository: NewsRepository
+) : ViewModel() {
+    val breakingNews : MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    var breakingNewsPage = 1
+    var breakingNewsResponse : NewsResponse? = null
+
+    val searchNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    var searchNewsPage = 1
+    var searchNewsResponse : NewsResponse? = null
+
+    init {
+        getBreakingNews("us")
+    }
+
+    fun getBreakingNews(countryCode : String) = viewModelScope.launch {
+        breakingNews.postValue(Resource.Loading())
+        val response = newsRepository.getBreakingNews(countryCode, breakingNewsPage)
+        breakingNews.postValue(handleBreakingNewsResponse(response))
+
+    }
+
+    fun searchNews(searchQuery : String) = viewModelScope.launch {
+        searchNews.postValue(Resource.Loading())
+        val response = newsRepository.searchNews(searchQuery, searchNewsPage)
+        searchNews.postValue(handleSearchNewsResponse(response))
+    }
+
+    private fun handleBreakingNewsResponse(response: Response<NewsResponse>) : Resource<NewsResponse>{
+        if (response.isSuccessful){
+            response.body()?.let { resultResponse ->
+                breakingNewsPage++;
+                if (breakingNewsResponse == null){
+                    breakingNewsResponse = resultResponse
+                } else {
+                    val oldArticle = breakingNewsResponse?.articles
+                    val newArtcles = resultResponse.articles
+                    oldArticle?.addAll(newArtcles)
+                }
+                return Resource.Success(breakingNewsResponse ?: resultResponse)
+            }
+        }
+        return Resource.Error(response.message())
+    }
+
+    private fun handleSearchNewsResponse(response : Response<NewsResponse>) : Resource<NewsResponse>{
+        if (response.isSuccessful){
+            response.body()?.let { resultResponse ->
+                searchNewsPage++;
+                if (searchNewsResponse == null){
+                    searchNewsResponse = resultResponse
+                } else {
+                    val oldArticle = searchNewsResponse?.articles
+                    val newArtcles = resultResponse.articles
+                    oldArticle?.addAll(newArtcles)
+                }
+                return Resource.Success(searchNewsResponse ?: resultResponse)
+            }
+        }
+        return Resource.Error(response.message())
+    }
+
+    fun saveArticle(article : Article) = viewModelScope.launch {
+        newsRepository.upsert(article)
+    }
+
+    fun getSaveNews() = newsRepository.getSavedNews()
+
+    fun deleteArticle(article: Article) = viewModelScope.launch {
+        newsRepository.deleteArticle(article)
+    }
+}
---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Contants.kt
@@ -0,0 +1,11 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Contants {
+    companion object{
+        const val API_KEY = "ff684b5c925b4d638279c915d8027253"
+        const val BASE_URL = "https://newsapi.org/"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE  = 20
+    }
+
+}
